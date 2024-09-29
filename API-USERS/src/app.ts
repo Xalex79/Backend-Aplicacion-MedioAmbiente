4d94fe3736bfa9d0c0c33a31e6a87227b43ad223
Alex <alexeltren@gmail.com>
2024-09-29 20:20:47 +0200

    Request en local funcionando
    Puedo hacer POST y GET en el servidor local y crear y ver usuarios

--- API-USERS/src/app.ts
+++ API-USERS/src/app.ts
@@ -1,10 +1,10 @@
-import app from "@server/server"
-import dotenv from 'dotenv'
+import app from "@server/server";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const port =  process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
-app.listen(port, ()=> {
-    console.log(`Server is running on port ${port}`)
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
