be50438720c94992c32e1833f3185f47e2c94f66
Alex <alexeltren@gmail.com>
2024-09-29 19:05:58 +0200

    Esqueleto Backend Funcionando
    Api is Healthy!!!

---
+++ API-USERS/src/routes/routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+const router = Router()
+
+export default () => {
+    router.get("/health", (req, res) => {
+        res.send("Api is Healthy!!!");
+    });
+    
+    return router;
+};
+
