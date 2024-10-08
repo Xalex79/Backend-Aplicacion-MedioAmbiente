4c2e66fc7e20bc5bb350f5e1752cf7c7f9f05942
Alex <alexeltren@gmail.com>
2024-10-08 11:30:31 +0200

    Ahora Frontend, Backend, MongoDB BBDD local en mismo Contenedor Docker

--- API-USERS/src/config/mongodb.ts
+++ API-USERS/src/config/mongodb.ts
@@ -1,16 +1,24 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+// Cargar las variables de entorno desde el archivo .env
 dotenv.config();
 
-const mongoDbURL = process.env.MONGODB_URL_STRING as string;
+// Asegurarse de que la variable MONGODB_URL_LOCAL esté definida
+const mongoDbURL = process.env.MONGODB_URL_LOCAL as string;
+
+if (!mongoDbURL) {
+  throw new Error("La URL de MongoDB no está definida en el archivo .env");
+}
 
 export default (async () => {
   try {
+    // Conectar a MongoDB sin las opciones 'useNewUrlParser' y 'useUnifiedTopology'
     await mongoose.connect(mongoDbURL);
     console.log("Mongodb Connected!!!");
   } catch (error) {
-    console.log("error :>> ", error);
-    process.exit(1);
+    // Mostrar el error en caso de que falle la conexión
+    console.error("Error al conectar a MongoDB :>> ", error);
+    process.exit(1); // Salir del proceso con error
   }
 })();
